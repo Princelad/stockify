2dd4670c11badfb43b86e2c32d5c80d84094bf46
dev patel <dspatel0006@gmail.com>
2025-08-22 13:50:03 +0530

    some changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import AuthCallback from "./pages/AuthCallback.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { ProtectedRoute } from "./components/ProtectedRoute.tsx";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
